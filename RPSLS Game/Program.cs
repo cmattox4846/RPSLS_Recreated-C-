36a4b25005a5f23fa3d7c8cd72a6f189c408539d
cmattox4846 <cmattox4846@gmail.com>
2021-11-16 16:31:20 -0500

    intital layout of classes and methods

--- RPSLS Game/Program.cs
+++ RPSLS Game/Program.cs
@@ -1,4 +1,169 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Human player = new Human("Chris");
 
+static void Main(string[] args)
+{
+
+}
+public class GameBoard
+{
+    Human player = new("Chris");
+    Ai computerPlayer = new("Megaton");
+}
+   
+
+
+   public void run_game()
+    {
+        display_welcome();
+        game_mode();
+    }
+
+
+   public string display_welcome() 
+    {
+        Console.WriteLine("Welcome to RPSLS (Rock, Paper, Scissors, Lizard, Spock");
+        Console.WriteLine("RPSLS is just like Rock Paper Scissors, each player has to choose any one variable Rock, Paper, Scissors, Lizard or Spock.\n Once the each player has chosen there vaiable, the choicces will be revealed and compared and winner will be decleared.\n The winner will be decided by the chart below\n * Rock crushes Scissors  ---  Scissors cuts Paper --- Paper covers Rock --- Rock crushes Lizard --- Lizard poisons Spock \n Spock smashes Scissors --- Scissors decapitates Lizard --- Lizard eats Paper --- Paper disproves Spock --- Spock vaporizes Rock");
+    }
+
+
+    public void game_mode():
+        game_mode = int(input("1 - To play against a another human \n2 - To Play against the computer\n"))
+
+
+        number_of_games = self.number_of_games()
+        player_one_name = input('\nPlease enter your name: ')
+        if game_mode == 1: 
+            player_two_name = input('\nPlease enter your name: ')
+            self.player_two = Human(player_two_name)
+            self.play_game(number_of_games, player_one_name)
+
+
+        elif game_mode == 2:
+           self.player_two = AI("Iron Man")
+           self.play_game(number_of_games, player_one_name)
+
+
+        else:
+            print("Please only pick 1 or 2!")
+            self.game_mode()
+
+
+    def number_of_games(self):
+        number_of_games = int(input("\n Please choose number of games up to 20: \n"))
+        amount_to_win = math.floor(number_of_games / 2) + 1
+
+        return amount_to_win
+
+    def play_game(self, number_of_games, player_one_name):
+        
+        player_1_score = 0
+        player_2_score = 0
+        game_over = False
+        choice1 = ""
+        choice2 = ""
+        player_2_name = self.player_two.name
+
+
+        while game_over == False:
+            print(f'\n{player_one_name}, It is your turn.')
+            choice1 = self.player_one.choose_gesture()
+            print(f'\n{player_2_name}, It is your turn.')
+            choice2 = self.player_two.choose_gesture()
+            print(f'\n{player_one_name} has {choice1} VS {player_2_name} has {choice2}')
+            winner = self.determine_winner(choice1, choice2)
+            if winner == choice1:
+                player_1_score += 1
+                print(f"\n {player_one_name} has a score of {player_1_score}")
+                print(f" {player_2_name} has a score of {player_2_score}")
+
+            elif winner == choice2:
+                player_2_score += 1
+                print(f"\n {player_one_name} has a score of {player_1_score}")
+                print(f" {player_2_name} has a score of {player_2_score}")
+
+
+
+            if player_1_score == number_of_games:
+                self.display_winner(player_one_name)
+                game_over = True
+            elif player_2_score == number_of_games:
+                self.display_winner(player_2_name)
+                game_over = True
+
+
+
+    def determine_winner(self, choice1, choice2):
+        if choice1 == 'rock' and choice2 == 'scissors':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice1 == 'scissors' and choice2 == 'paper':
+             print(f"{choice1} beats {choice2}")
+             return choice1
+        elif choice1 == 'paper' and choice2== 'rock':
+             print(f"{choice1} beats {choice2}")
+             return choice1
+        elif choice1 == 'rock' and choice2== 'lizard':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice1 == 'lizard' and choice2== 'spock':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice1 == 'spock' and choice2== 'scissors':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice1 == 'scissors' and choice2== 'lizard':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice1 == 'lizard' and choice2== 'paper':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice1 == 'paper' and choice2== 'spock':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice1 == 'spock' and choice2== 'rock':
+            print(f"{choice1} beats {choice2}")
+            return choice1
+        elif choice2 == 'rock' and choice1 == 'scissors':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'scissors' and choice1== 'paper':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'paper' and choice1== 'rock':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'rock' and choice1== 'lizard':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'lizard' and choice1== 'spock':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'spock' and choice1== 'scissors':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'scissors' and choice1== 'lizard':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'lizard' and choice1== 'paper':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'paper' and choice1== 'spock':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        elif choice2 == 'spock' and choice1== 'rock':
+            print(f"{choice2} beats {choice1}")
+            return choice2
+        else:
+            print(f'Its a draw! You both choose {choice1}')
+
+
+
+
+    def display_winner(self, player):
+        print(f"\nCongrats {player} you have won the game!")
+        answer = input('\nWould you like to play again?\n 1- Play Again\n 2- End Game\n Enter Choice: ')
+        if answer == "1":
+             self.run_game()
+        else:
+            sys.exit()
 Console.WriteLine(player);
--- RPSLS Game/Robot.cs
+++ RPSLS Game/Robot.cs
@@ -1,39 +1,38 @@
 ﻿using System;
+using System;
 
-namespace RPSLS Game
-
-public class Human
+public class Ai
 {
-    
+
     public string Choice;
     public string name;
-    
-    public Human(string playerName)
+
+    public Ai(string playerName)
     {
         name = playerName;
         Choice = ChooseGesture();
     }
     public string ChooseGesture()
     {
-        Console.WriteLine("Please choose a number below\nPick 1 for rock\npick 2 for paper\npick 3 for scissors\npick 4 for lizard\npick 5 for spock\n");
-        string user_input = Console.ReadLine();
-        if (user_input == "1")
+        Random rnd = new Random();
+        int AIChoice = rnd.Next(1, 6);
+        if (AIChoice == 1)
         {
             return "rock";
         }
-        else if (user_input == "2")
+        else if (AIChoice == 2)
         {
             return "paper";
         }
-        else if (user_input == "3")
+        else if (AIChoice == 3)
         {
             return "scissors";
         }
-        else if (user_input == "4")
+        else if (AIChoice == 4)
         {
             return "lizard";
         }
-        else if (user_input == "5")
+        else if (AIChoice == 5)
         {
             return "spock";
         }
@@ -41,7 +40,7 @@ public class Human
         {
             return "choose valid number";
         }
-        
+
     }
 
 }
